6e51e837eaf95048def7ec53a755d8701283de93
grant@bemedia.com.au <grant@bemedia.com.au>
2023-06-28 16:16:16 +0200

    add skeleton base

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,49 +1,83 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	import '../theme.postcss';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import {
+		AppBar,
+		AppShell,
+		Avatar,
+		Drawer,
+		Toast,
+		drawerStore,
+		toastStore,
+		type ToastSettings,
+		Modal,
+		type ModalComponent
+	} from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/Navigation.svelte';
+	import CardModal from '$lib/components/CardModal.svelte';
+
+	const t: ToastSettings = {
+		message: 'menu opened'
+	};
+
+	function drawerOpen() {
+		drawerStore.open();
+		toastStore.trigger(t);
+	}
+
+	const modalComponentRegistry: Record<string, ModalComponent> = {
+		// Custom Modal 1
+		card: {
+			ref: CardModal
+		}
+	};
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
+<Toast position="br" />
+<Modal components={modalComponentRegistry} />
+<Drawer>
+	<Navigation />
+</Drawer>
+
+<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
+	<svelte:fragment slot="header"
+		><AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
+				<a href="/"><strong class="text-4xl">Basket</strong></a></svelte:fragment
+			>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
+				<button class="md:hidden btn btn-sm" on:click={drawerOpen}>
+					<span>
+						<svg viewBox="0 0 100 100" class="fill-token w-4 h-4">
+							<rect width="100" height="20" />
+							<rect y="40" width="100" height="20" />
+							<rect y="80" width="100" height="20" />
+						</svg>
+					</span>
+				</button>
+				<Avatar
+					initials="JD"
+					background="bg-primary-500"
+					width="w-10"
+					class="hidden md:block"
+				/></svelte:fragment
+			>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
+	<svelte:fragment slot="sidebarLeft"><Navigation /></svelte:fragment>
+
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
 </AppShell>
